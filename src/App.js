91dc0b58a957f7e2347442c223b80b50aaffbe79
Pedro Scarton <43622582+PedroScarton@users.noreply.github.com>
2020-12-01 17:35:38 -0300

    start app components/styles
    Button
    Title
    Start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Start from './Start/pages/Start';
+import './App.css';
+
 const App = () => {
   return (
     <React.Fragment>
-        
+      <main>
+        <Start/>
+      </main>
     </React.Fragment>
   );
 }
