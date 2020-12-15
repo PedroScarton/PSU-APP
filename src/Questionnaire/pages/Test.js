49aee7908660811ad98e3c35312a1540a71d570f
Pedro Scarton <43622582+PedroScarton@users.noreply.github.com>
2020-12-15 20:29:43 -0300

    dummy questions remove

--- src/Questionnaire/pages/Test.js
+++ src/Questionnaire/pages/Test.js
@@ -9,81 +9,6 @@ import Solutionary from './Solutionary'
 import ErrorModal from '../../Shared/components/UIElements/ErrorModal'
 import LoadingSpinner from '../../Shared/components/UIElements/LoadingSpinner';
 
-const DUMMY_QUESTIONS = [
-    {
-        id: 1,
-        statement: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero',
-            '<img src={} alt=""/>',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero'
-        ],
-        option1: 'Esta es la opción 1',
-        option2: 'Esta es la opción 2',
-        option3: 'Esta es la opción 3',
-        option4: 'Esta es la opción 4',
-        option5: 'Esta es la opción 5',
-        difficulty: "MEDIUM",
-        answer: {
-            id: 1,
-            correctOption: 2,
-            description: [
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero',
-                '<img/>',
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero'
-            ]
-        }
-
-    },
-    {
-        id: 2,
-        statement: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero',
-            '<img src={} alt=""/>',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero'
-        ],
-        option1: 'Esta es la opción 1',
-        option2: 'Esta es la opción 2',
-        option3: 'Esta es la opción 3',
-        option4: 'Esta es la opción 4',
-        option5: 'Esta es la opción 5',
-        difficulty: "MEDIUM",
-        answer: {
-            id: 1,
-            correctOption: 2,
-            description: [
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero',
-                '<img/>',
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero'
-            ]
-        }
-
-    },
-    {
-        id: 3,
-        statement: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero',
-            '<img src={} alt=""/>',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero'
-        ],
-        option1: 'Esta es la opción 1',
-        option2: 'Esta es la opción 2',
-        option3: 'Esta es la opción 3',
-        option4: 'Esta es la opción 4',
-        option5: 'Esta es la opción 5',
-        difficulty: "MEDIUM",
-        answer: {
-            id: 1,
-            correctOption: 2,
-            description: [
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero',
-                '<img/>',
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit sapien metus, in faucibus sem rutrum malesuada. Vivamus eleifend justo libero'
-            ]
-        }
-
-    }
-]
-
 const Test = () => {
 
     const auth = useContext(AuthContext);
@@ -98,24 +23,23 @@ const Test = () => {
             try {
                 const respondeData = await sendRequest(
                     process.env.REACT_APP_BACKEND_URL + `/test`,
-                    'PATCH',
+                    'POST',
                     JSON.stringify({
-
+                        numberOfQuestions: 20
                     }),
                     {
                         'Content-Type': 'application/json', Authorization: 'Bearer ' + auth.token
                     }
                 );
-                return respondeData;
+                setEnsayo(respondeData);
+                setStage('ensayo');
+
             } catch (err) {
 
             }
         }
-        const newEnsayo = getEnsayo();
-        setEnsayo(newEnsayo)
-        setStage('ensayo');
-
-    }, [setStage, sendRequest]);
+        getEnsayo();
+    }, [setStage, sendRequest, auth]);
 
     const nextStageHandler = (value) => {
         setStage(value)
@@ -139,7 +63,7 @@ const Test = () => {
                 )
             }
             {
-                stage === 'ensayo' && (
+                ensayo && stage === 'ensayo' && (
                     <Questionnaire
                         ensayo={ensayo.test}
                         updateQuestions={updateQuestionsHandler}
@@ -147,7 +71,7 @@ const Test = () => {
                 )
             }
             {
-                stage === 'resume' && (
+                ensayo && stage === 'resume' && (
                     <Resume
                         ensayo={ensayo}
                         updateQuestions={updateQuestionsHandler}
@@ -156,7 +80,7 @@ const Test = () => {
                 )
             }
             {
-                stage === 'solutions' && (
+                ensayo && stage === 'solutions' && (
                     <Solutionary
                         ensayo={ensayo.test} />
                 )
